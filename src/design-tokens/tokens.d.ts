58ac92b0f4ba8042fd65ae947adced4abf0e72e7
suzuken0424 <s.kenta0424@gmail.com>
2025-01-10 21:52:57 +0900

    test

---
+++ src/design-tokens/tokens.d.ts
@@ -0,0 +1,24 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+export default tokens;
+
+declare interface DesignToken {
+  value?: any;
+  type?: string;
+  comment?: string;
+  name?: string;
+  themeable?: boolean;
+  attributes?: Record<string, unknown>;
+  [key: string]: any;
+}
+
+declare const tokens: {
+  red: DesignToken;
+  "hover-red": DesignToken;
+  black: DesignToken;
+  tokenSetOrder: {
+    "0": DesignToken;
+  };
+};
---
+++ src/design-tokens/tokens.js
@@ -0,0 +1,69 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  red: {
+    value: "#e64026",
+    type: "color",
+    filePath: "design-tokens/02-dest/tokens.json",
+    isSource: true,
+    original: {
+      value: "#e64026",
+      type: "color",
+    },
+    name: "Red",
+    attributes: {
+      category: "red",
+    },
+    path: ["red"],
+  },
+  "hover-red": {
+    value: "#e4c9c3",
+    type: "color",
+    filePath: "design-tokens/02-dest/tokens.json",
+    isSource: true,
+    original: {
+      value: "#e4c9c3",
+      type: "color",
+    },
+    name: "HoverRed",
+    attributes: {
+      category: "hover-red",
+    },
+    path: ["hover-red"],
+  },
+  black: {
+    value: "#ffffff",
+    type: "color",
+    filePath: "design-tokens/02-dest/tokens.json",
+    isSource: true,
+    original: {
+      value: "#ffffff",
+      type: "color",
+    },
+    name: "Black",
+    attributes: {
+      category: "black",
+    },
+    path: ["black"],
+  },
+  tokenSetOrder: {
+    0: {
+      value: "global",
+      type: "other",
+      filePath: "design-tokens/02-dest/tokens.json",
+      isSource: true,
+      original: {
+        value: "global",
+        type: "other",
+      },
+      name: "TokenSetOrder0",
+      attributes: {
+        category: "tokenSetOrder",
+        type: "0",
+      },
+      path: ["tokenSetOrder", "0"],
+    },
+  },
+};
